2671a3f891884332127181c0534865e39c2093e4
I-Ray <chinnivenkatapavan@gmail.com>
2025-01-17 15:24:48 +0530

    Learning terraform

--- vm-create-v1/main.tf
+++ vm-create-v1/main.tf
@@ -1,7 +1,11 @@
+data "azurerm_resource_group" "example" {
+  name = "project-setup-1"
+}
+
 resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+  name                  = "test-vm"
+  location              = data.azurerm_resource_group.example.location
+  resource_group_name   = data.azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
