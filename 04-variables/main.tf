86363a89447874c7b9624be3b71bc27749a1f46d
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-18 07:09:21 +0530

    Added New concept in my list

---
+++ 04-variables/main.tf
@@ -0,0 +1,52 @@
+variable "name" {
+    default = "Hello"
+}
+
+variable "n1" {
+    default = 2
+}
+
+variable "b1"{
+    default = true
+}
+
+#Data types that terraform supports
+#1.String
+#2. Number
+#3. Boolean
+
+# String only require quotes, That to be only double quotes. Terraform does not have single quotes support.
+
+variable "v1" {
+    default = "Hello"
+    }
+
+variable "v2" {
+    default = [
+        "Hello",
+        2,
+        false
+        ]
+    }
+
+variable "v3" {
+    deafult = {
+        course = "devops"
+        cloud = "azure"
+        }
+    }
+
+# variable type that terraform supports
+# 1. plain
+# 2. List (A key having multiple values, and each value can be a different data type)
+# 3. map (A key having again keys and values)
+
+## Access your variable
+output "v1" {
+    value = var.v1
+    }
+
+# While accessing variable, if we have some string combination then we can access it with ${}
+output "v2" {
+    value = "${var.v1} - John"
+    }
