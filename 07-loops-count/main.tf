08f814abd691942d63258b74dbae7ac9d573ba99
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-18 20:55:15 +0530

    Added New concept in my list

--- 07-loops-count/main.tf
+++ 07-loops-count/main.tf
@@ -1,3 +1,14 @@
 resource "null_resource" "demo"{
-    count = 10
+    count = length(var.demo)
+
+    provisioner "local-exec"{
+        command = "echo ${var.demo[count.index]}"
+        }
+    }
+
+variable "demo" {
+    default = [
+        "apple",
+        "banana"
+        ]
     }
