afdad8cefc91c499c56c86b86555a90f0c2b836b
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-18 06:45:27 +0530

    Added New concept in my list

--- 03-resource/main.tf
+++ 03-resource/main.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+    features {}
+    subscription_id = "b3406e49-0e16-456d-a3bb-c324f1dfb0d5"
+     }
+
 data "azurerm_resource_group" "example" {
   name = "project-setup-1"
 }
