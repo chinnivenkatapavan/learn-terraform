8abecd7005fdd5840ab04469654fb5cf05d0c0d6
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-19 13:34:10 +0530

    Added New concept in my list

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,9 +1,49 @@
-module "frontend"{
+module "component"{
+    for_each = var.component
     source = "./vm"
-    component = "frontend"
+    component = each.value["name"]
 }
 
-module "catalogue"{
-    source = "./vm"
-    component = "catalogue"
-}
+
+variable "component" {
+    default = {
+
+        frontend = {
+            name = "frontend"
+            }
+        mongodb = {
+            name = "mongodb"
+            }
+        catalogue = {
+            name = "catalogue"
+            }
+        user = {
+            name = "user"
+            }
+        cart = {
+            name = "cart"
+            }
+        mysql = {
+            name = "mysql"
+            }
+        shipping = {
+            name = "shipping"
+            }
+        payment = {
+            name = "payment"
+            }
+        rabbitmq = {
+            name = "rabbitmq"
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+    }
