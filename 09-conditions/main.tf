905ccc2c2989189d54ff787846c4aaf1a74ad7ea
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-01-22 21:40:51 +0530

    Added New concept in my list

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -3,3 +3,9 @@ variable "a"{}
 output "fg"{
     value = var.a > 10 ? "a is greater than 10" : "a is less than 10"
     }
+
+# These conditions with combination of loop can be used to determine whether a resource to run or not.value
+
+resource "null_resource" "test" {
+    count = var.a > ? 1 : 0
+    }
