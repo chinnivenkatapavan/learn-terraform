c4ca90e3dbf165a021774a6b78abc0e7d4f3c30c
I-Ray <chinnivenkatapavan@gmail.com>
2025-01-23 13:02:22 +0530

    updated terraform

--- 10-remote-state/main.tf
+++ 10-remote-state/main.tf
@@ -1,7 +1,5 @@
 terraform {
-  backend "azurerm"
-  {
-
+  backend "azurerm" {
     resource_group_name  = "project-setup-1"
     storage_account_name = "d82tfstate"
     container_name       = "tfstates"
