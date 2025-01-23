9dd447d8988501d953d04ae7edad604f97e5a544
I-Ray <chinnivenkatapavan@gmail.com>
2025-01-23 12:58:42 +0530

    updated terraform

--- 10-remote-state/main.tf
+++ 10-remote-state/main.tf
@@ -1,12 +1,14 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "project-setup-1"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "d82tfstate"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstates"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "example.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  backend "azurerm"
+  {
+
+    resource_group_name  = "project-setup-1"
+    storage_account_name = "d82tfstate"
+    container_name       = "tfstates"
+    key                  = "example.tfstate"
   }
 }
 
-output"test" {
-    test = "hello"
+output "test" {
+    tests = "hello"
     }
