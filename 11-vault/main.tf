8a4c1c759b450d6dd9d820e03c84eed903f48da7
I-Ray <chinnivenkatapavan@gmail.com>
2025-01-24 17:54:28 +0530

    updated terraform

--- 11-vault/main.tf
+++ 11-vault/main.tf
@@ -5,7 +5,7 @@ provider "vault" {
   #
   # This will default to using $VAULT_ADDR
   # But can be set explicitly
-   address = "https://vault.azdevops2704.online:8200"
+   address = "http://vault.azdevops2704.online:8200"
    token   = var.token
 }
 
